9bbe2436cf8d57c79ed390f9e971be68a94698b3
yan <13186908+gitee_002@user.noreply.gitee.com>
2024-07-18 23:02:23 +0800

    fix-testvite

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  build: {
+    rollupOptions: {
+      external: [
+        "./list/hsk1.json",
+        "./list/hsk2.json",
+        "./list/hsk3.json",
+        "./list/hsk4.json",
+        "./list/hsk5.json",
+        "./list/hsk6.json",
+        "./list/hsk7-9.json",
+        "./list/allchar.json",
+        "./list/chengyu.json",
+      ],
+    },
+  },
+};
