70c35ae1f6886598d05bc3abc472293e4082ca04
yan <13186908+gitee_002@user.noreply.gitee.com>
2024-07-19 16:47:43 +0800

    feat-sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
