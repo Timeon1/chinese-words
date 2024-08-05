c354245298fd56c8710f0b872b7ef57ae95668c6
yan <13186908+gitee_002@user.noreply.gitee.com>
2024-08-05 15:20:05 +0800

    feat-xiehouyu

--- src/content/allFile.ts
+++ src/content/allFile.ts
@@ -1,9 +1,9 @@
 import hsk1 from "public/list/hsk1.json";
 // import hsk2 from "public/list/hsk2.json";
 // import hsk3 from "public/list/hsk3.json";
-import hsk4 from "public/list/hsk4.json";
-import hsk5 from "public/list/hsk5.json";
-import hsk6 from "public/list/hsk6.json";
+// import hsk4 from "public/list/hsk4.json";
+// import hsk5 from "public/list/hsk5.json";
+// import hsk6 from "public/list/hsk6.json";
 import hsk7 from "public/list/hsk7-9.json";
 // import allchar from "public/list/allchar.json";
 import chengyu from "public/list/chengyu.json";
@@ -13,6 +13,6 @@ import xiehouyu from "public/list/xiehouyu.json"
 //   allword: [...hsk1, ...hsk2, ...hsk3, ...hsk4, ...hsk5, ...hsk6, ...hsk7],
 //   fileList: [hsk1, hsk2, hsk3, hsk4, hsk5, hsk6, hsk7],
 // };
-export const allword = [...hsk1.data,  ...hsk4.data, ...hsk5.data, ...hsk6.data, ...hsk7.data,  ...chengyu.data, ...xiehouyu.map(item=> `${item.riddle}-${item.answer}`)]
-console.log('allword.length', allword.length)
+// export const allword = [...hsk1.data,  ...hsk4.data, ...hsk5.data, ...hsk6.data, ...hsk7.data,  ...chengyu.data, ...xiehouyu.map(item=> `${item.riddle}-${item.answer}`)]
+export const allword = [...hsk1.data,  ...chengyu.data, ...xiehouyu.data]
 // export const fileList = [hsk1, hsk2, hsk3, hsk4, hsk5, hsk6, hsk7]
